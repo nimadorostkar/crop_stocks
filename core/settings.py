5d759e53e255530a806437608a16973ef7ef062b
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-02 00:09:06 +0430

    ACCOUNT_FORMS to settings

--- core/settings.py
+++ core/settings.py
@@ -173,6 +173,21 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 
+ACCOUNT_FORMS = {
+    'login': 'allauth.account.forms.LoginForm',
+    'signup': 'allauth.account.forms.SignupForm',
+    'add_email': 'allauth.account.forms.AddEmailForm',
+    'change_password': 'allauth.account.forms.ChangePasswordForm',
+    'set_password': 'allauth.account.forms.SetPasswordForm',
+    'reset_password': 'allauth.account.forms.ResetPasswordForm',
+    'reset_password_from_key': 'allauth.account.forms.ResetPasswordKeyForm',
+    'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
+    "signup": "spybook.users.forms.SpyBookSignupForm",
+}
+
+
+
+
 ACCOUNT_AUTHENTICATION_METHOD = ("username")
 ACCOUNT_EMAIL_VERIFICATION = ("none")
 #ACCOUNT_EMAIL_VERIFICATION = ('mandatory')
