2a3e2fd10d0c3e0c44523f834d07332950a6a8e1
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-02 12:46:47 +0430

    ggg

--- core/settings.py
+++ core/settings.py
@@ -173,7 +173,7 @@ SOCIALACCOUNT_PROVIDERS = {
 ACCOUNT_FORMS = {
     'login': 'allauth.account.forms.LoginForm',
     #'signup': 'allauth.account.forms.SignupForm',
-    "signup": "dashboard.forms.ProfileForm", # custom signup form
+    "signup": "dashboard.forms.UserForm", # custom signup form
     'add_email': 'allauth.account.forms.AddEmailForm',
     'change_password': 'allauth.account.forms.ChangePasswordForm',
     'set_password': 'allauth.account.forms.SetPasswordForm',
