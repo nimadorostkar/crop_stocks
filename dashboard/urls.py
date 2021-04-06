0672dc4de18c863a838ec266a2278563e12e58c6
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-06 17:04:31 +0430

    add money_req to urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('payment/', views.payment, name='payment'),
     path('ticket/', views.ticket, name='ticket'),
+    path('money_req/', views.money_req, name='money_req'),
 ]
