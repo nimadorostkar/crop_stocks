21f16347166052b9342a7b34d8c4c63433e4e3a6
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-06 14:57:05 +0430

    edit template for tickets

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -7,14 +7,14 @@ from . import models
 admin.site.site_header= " Crop "
 admin.site.site_title= " Crop "
 
-#'ticket_number' ,
+
 
 class NoticeAdmin(admin.ModelAdmin):
 	list_display = ('title', 'created_on')
 class Submitted_filesAdmin(admin.ModelAdmin):
 	list_display = ('title', 'created_on')
 class TicketAdmin(admin.ModelAdmin):
-	list_display = ('user_name', 'title','descriptions','created_on','status','case')
+	list_display = ('ticket_number' ,'user_name', 'title','descriptions','created_on','status','case')
 class PaymentAdmin(admin.ModelAdmin):
 	list_display = ('user_name', 'image_tag','descriptions','created_on')
 class ProfileAdmin(admin.ModelAdmin):
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.html import format_html
 from django.dispatch import receiver
 from django.db import models
-
+import uuid
 
 
 
@@ -51,7 +51,7 @@ class Notice(models.Model):
 #------------------------------------------------------------------------------
 class Ticket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name = "کاربر")
-    ticket_number = models.Charfield( max_length = 10, blank=True, editable=False, unique=True, default=create_new_ref_number() )
+    ticket_number = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=300,null=True, blank=True,verbose_name = " عنوان ")
     descriptions = models.TextField(max_length=800,null=True, blank=True,verbose_name = "توضیحات")
     CHOICES1 = ( ('Ticket','Ticket'), ('Answered','Answered') )
@@ -60,7 +60,6 @@ class Ticket(models.Model):
     case = models.CharField(max_length=20,choices=CHOICES2,default='🔴New',verbose_name = "حالت")
     updated_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
-
 
     def user_name(self):
           return str(self.user)
